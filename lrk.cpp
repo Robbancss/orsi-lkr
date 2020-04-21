c7c874b3a49ae36b1bdb001a4d9061aaed0798a3
robert.panczel <robi.panczel@gmail.com>
2020-04-21 20:21:53 +0200

    Passed the tests

--- lrk.cpp
+++ lrk.cpp
@@ -6,35 +6,38 @@
 #include <thread>
 #include <future>
 
-void nyomtat(std::vector<std::vector<std::pair<int, int>>> b, std::string oSample, int i, int j, std::string& result);
+void nyomtat(std::vector<std::vector<std::pair<int, int>>>& b, std::string& oSample, int i, int j, std::string& result);
 
 std::string lkr(std::string oSample, std::string compareSample)
 {
     std::vector<std::vector<int> > c;
     std::vector<std::vector<std::pair<int, int>>> b;
 
+    oSample = "." + oSample;
+    compareSample = "-" + compareSample;
+
     int oSize = oSample.size();
     int cSize = compareSample.size();
 
-    c.resize(oSample.size());
-    b.resize(oSample.size());
-    for (int i = 0; i < oSample.size(); i++)
+    c.resize(oSize);
+    b.resize(oSize);
+    for (int i = 0; i < oSize; i++)
     {
-        c[i].resize(compareSample.size());
-        b[i].resize(compareSample.size());
+        c[i].resize(cSize);
+        b[i].resize(cSize);
     }
     
-    for (int i = 1; i < oSample.size(); i++)
+    for (int i = 0; i < oSize; i++)
     {
         c[i][0] = 0;
     }
-    for (int j = 0; j < compareSample.size(); j++)
+    for (int j = 1; j < cSize; j++)
     {
         c[0][j] = 0;
     }
-    for (int i = 1; i < oSample.size(); i++)
+    for (int i = 1; i < oSize; i++)
     {
-        for (int j = 1; j < compareSample.size(); j++)
+        for (int j = 1; j < cSize; j++)
         {
             if (oSample[i] == compareSample[j])
             {
@@ -58,11 +61,11 @@ std::string lkr(std::string oSample, std::string compareSample)
     }
 
     std::string temp = "";
-    nyomtat(b, oSample, oSample.size()-1, compareSample.size()-1, temp);
+    nyomtat(b, oSample, oSize-1, cSize-1, temp);
     return temp;
 }
 
-void nyomtat(std::vector<std::vector<std::pair<int, int>>> b, std::string oSample, int i, int j, std::string& result)
+void nyomtat(std::vector<std::vector<std::pair<int, int>>>& b, std::string& oSample, int i, int j, std::string& result)
 {
     if (i == 0 || j == 0)
     {
@@ -112,20 +115,12 @@ int main()
         parsedInput.push_back(temp);
     }
 
-    // Write input.txt  to the console
-    for (size_t i = 0; i < parsedInput.size(); i++)
-    {
-        lkr(originalSample, parsedInput[i]);
-    }
-
     for (int i = 0; i < comparedSampleCounter; i++)
     {
         futureResults.push_back(std::async(std::launch::async, lkr, originalSample, parsedInput[i]));
     }
 
     std::vector<std::string> outPutStrings;
-
-
     for (int i = 0; i < comparedSampleCounter; i++) {
         outPutStrings.push_back(futureResults[i].get());
     }
